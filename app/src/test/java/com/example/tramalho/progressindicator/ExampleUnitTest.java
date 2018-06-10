a9f0caa44122ff1e00b9455484c7713a51c4822c
trama <thiagofacul@gmail.com>
2018-06-10 13:01:03 -0300

    - config project

--- app/src/test/java/com/example/tramalho/progressindicator/ExampleUnitTest.java
+++ app/src/test/java/com/example/tramalho/progressindicator/ExampleUnitTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <background_pressed href="http://d.android.com/tools/testing">Testing documentation</background_pressed>
  */
 public class ExampleUnitTest {
     @Test
