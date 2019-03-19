83d44085e0264ea6600d3ed6496569f6ae0634b0
tramalho <tramalho@ciandt.com>
2019-03-19 13:01:27 -0300

    - TextView Selector - Change image programatically

--- app/src/main/java/com/example/tramalho/progressindicator/MainActivity.java
+++ app/src/main/java/com/example/tramalho/progressindicator/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.tramalho.progressindicator;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private StepsCustomProgress steps;
+    private TextView textViewSelector;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         final Button backButton = findViewById(R.id.back_id);
         final Button nextButton = findViewById(R.id.next_button_id);
         CheckBox checkBox = findViewById(R.id.checkbox1);
+        textViewSelector = findViewById(R.id.text_selector);
+        imageView = findViewById(R.id.imageviewId);
 
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -50,5 +57,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void backClick(View view) {
         steps.back();
+    }
+
+    public void versionClick(View view) {
+        String release = Build.VERSION.RELEASE;
+        int sdkVersion = Build.VERSION.SDK_INT;
+
+        Toast.makeText(this, "Android SDK: " + sdkVersion + " (" + release + ")", Toast.LENGTH_SHORT).show();
+
+        textViewSelector.setEnabled(!textViewSelector.isEnabled());
+
+        imageView.setImageResource(textViewSelector.isEnabled() ? R.mipmap.ic_launcher : R.mipmap.ic_launcher_round);
     }
 }
